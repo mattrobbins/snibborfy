b2359d8eb818b19a8eff9a64178843cec08f546b
Matt Robbins <robbinsmk@gmail.com>
2015-04-13 16:05:17 -0600

    form-based workflow feature completed

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,5 @@
 class OrganizationsController < ApplicationController
+include OrganizationsHelper
 
   def index
     @organizations = Organization.all
@@ -8,4 +9,38 @@ class OrganizationsController < ApplicationController
     @organization = Organization.find(params[:id])
   end
 
+  def new
+    @organization = Organization.new
+  end
+
+  def create
+    @organization = Organization.new(organization_params)
+    @organization.save
+
+    flash.notice = "Organization '#{@organization.company_name}' was created!"
+
+    redirect_to organization_path(@organization)
+  end
+
+  def destroy
+    @organization = Organization.find(params[:id])
+    @organization.destroy
+
+    flash.notice = "Organization '#{@organization.company_name}' was deleted!"
+
+    redirect_to action: "index"
+  end
+
+  def edit
+    @organization = Organization.find(params[:id])
+  end
+
+  def update
+    @organization = Organization.find(params[:id])
+    @organization.update(organization_params)
+
+    flash.notice = "Organization '#{@organization.company_name}' Updated!"
+
+    redirect_to organization_path(@organization)
+  end
 end
